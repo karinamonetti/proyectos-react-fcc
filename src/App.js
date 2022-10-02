95aa834d1f94370e0d9b7c2cee3dfe76d8a738dd
Kari Monetti <karinamonetti.dev@gmail.com>
2022-10-01 22:50:22 -0300

    agregué las props a testimonio

--- src/App.js
+++ src/App.js
@@ -5,8 +5,27 @@ function App() {
   return (
     <div className="App">
       <div className='contenedor'>
+
         <h1>Esto es lo que dicen nuestros alumnos de FreeCodeCamp</h1>
-        <Testimonio/>
+
+        <Testimonio 
+        nombre="Ernesto Benedictino"
+        ocupacion="Youtuber famoso"
+        imagen="1"
+        />
+
+        <Testimonio
+        nombre="Patricia Bonwikz"
+        ocupacion="Analista de datos"
+        imagen="2"
+        />
+
+        <Testimonio
+        nombre="Emma Stalone"
+        ocupacion="Ingeniera de Software"
+        imagen="3"
+        />
+
       </div>
       
     </div>
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,18 +1,17 @@
 import React from "react";
-import testimonio1 from "../imagenes/testimonio-1.jpg";
 import "../estilos/Testimonio.css"
 
-function Testimonio () {
+function Testimonio (props) {
 
     return(
         <>
             <div className="contenedor-testimonio">
 
-                <img className="img-testimonio" src={testimonio1} alt="youtuber de fama mundial"/>
+                <img className="img-testimonio" src={require(`../imagenes/testimonio-${props.imagen}.jpg`)} alt="youtuber de fama mundial"/>
 
                 <div className="contenedor-texto">
-                    <p className="nombre">Ernesto Benedictino</p>
-                    <p className="ocupacion">Youtuber de fama mundial</p>
+                    <p className="nombre">{props.nombre}</p>
+                    <p className="ocupacion">{props.ocupacion}</p>
                     <p className="testimonio">Hola, yo soy Ernesto y estoy contento con FreeCodeCamp. No se que, no se cuanto, no me interesa demasiado solo estoy probando a ver si esta mierda funciona o si tengo que meter css por todos lados... Okey, parece que si funcionó. Al fin!</p>
                 </div>
                 
