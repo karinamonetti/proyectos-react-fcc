29d7bc1a9826a9c3198b861de4d333cf21bd8239
Kari Monetti <karinamonetti.dev@gmail.com>
2022-10-01 21:51:42 -0300

    modifico componente testimonio

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,11 +1,21 @@
 import React from "react";
+import testimonio1 from "../imagenes/testimonio-1.jpg";
 
 function Testimonio () {
 
     return(
         <>
             <div className="contenedor">
-                <img className="img-testimonio" src=""/>
+
+                <img className="img-testimonio" src={testimonio1} alt="youtuber de fama mundial"/>
+
+                <div className="contenedor-texto">
+                    <p className="nombre">Ernesto Benedictino</p>
+                    <p className="ocupacion">Youtuber de fama mundial</p>
+                    <p className="testimonio">sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd 
+                    </p>
+                </div>
+                
             </div>
         </>
     );
