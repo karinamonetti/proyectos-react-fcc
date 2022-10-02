d16c68af31bff8e9d4b88ced1409c4dfbd70ce05
Kari Monetti <karinamonetti.dev@gmail.com>
2022-10-01 22:29:49 -0300

    terminé el primer componente, ahora faltan las props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Testimonio from './componentes/Testimonio';
 
 function App() {
   return (
     <div className="App">
+      <div className='contenedor'>
+        <h1>Esto es lo que dicen nuestros alumnos de FreeCodeCamp</h1>
+        <Testimonio/>
+      </div>
       
     </div>
   );
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,19 +1,19 @@
 import React from "react";
 import testimonio1 from "../imagenes/testimonio-1.jpg";
+import "../estilos/Testimonio.css"
 
 function Testimonio () {
 
     return(
         <>
-            <div className="contenedor">
+            <div className="contenedor-testimonio">
 
                 <img className="img-testimonio" src={testimonio1} alt="youtuber de fama mundial"/>
 
                 <div className="contenedor-texto">
                     <p className="nombre">Ernesto Benedictino</p>
                     <p className="ocupacion">Youtuber de fama mundial</p>
-                    <p className="testimonio">sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd 
-                    </p>
+                    <p className="testimonio">Hola, yo soy Ernesto y estoy contento con FreeCodeCamp. No se que, no se cuanto, no me interesa demasiado solo estoy probando a ver si esta mierda funciona o si tengo que meter css por todos lados... Okey, parece que si funcionó. Al fin!</p>
                 </div>
                 
             </div>
